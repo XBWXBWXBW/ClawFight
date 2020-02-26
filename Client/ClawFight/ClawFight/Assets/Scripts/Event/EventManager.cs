2674b7e4ddea8cabebb5ef2990209a6ce8f4b295
XBWXBWXBW <949030585@qq.com>
2020-02-26 11:18:52 +0800

    添加事件机制

---
+++ Client/ClawFight/ClawFight/Assets/Scripts/Event/EventManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager
+{
+    private interface IAction { }
+    private class ActionEvent<T> : IAction {
+        public Action<T> action;
+        public void RegistEvent(Action<T> _action){
+            action -= _action;
+            action += _action;
+        }
+        public void UnRegistEvent(Action<T> _action) {
+            action -= _action;
+        }
+        public void SendEvent(T param) {
+            action.Invoke(param);
+        }
+    }
+    private static EventManager _instance;
+    public static EventManager instance {
+        get {
+            if (_instance == null) {
+                _instance = new EventManager();
+            }
+            return _instance;
+        }
+    }
+    private Dictionary<string, IAction> actionTDict = new Dictionary<string, IAction>();
+    private Dictionary<string, Action> actionDict = new Dictionary<string, Action>();
+    public void RegistEventT<T>(string eventName, Action<T> action) {
+        if (!actionTDict.ContainsKey(eventName))
+        {
+            ActionEvent<T> at_new = new ActionEvent<T>();
+            actionTDict.Add(eventName, at_new);
+        }
+        ActionEvent<T> at = actionTDict[eventName] as ActionEvent<T>;
+        at.RegistEvent(action);
+    }
+    public void UnRegisEventT<T>(string eventName, Action<T> action) {
+        if (actionTDict.ContainsKey(eventName)) {
+            ActionEvent<T> at = actionTDict[eventName] as ActionEvent<T>;
+            at.UnRegistEvent(action);
+            if (at.action == null) {
+                actionTDict.Remove(eventName);
+            }
+        }
+    }
+    public void SendEventT<T>(string eventName, T param) {
+        if (actionTDict.ContainsKey(eventName)) {
+            (actionTDict[eventName] as ActionEvent<T>).SendEvent(param);
+        }
+    }
+    public void RegistEvent(string eventName, Action action) {
+        if (!actionDict.ContainsKey(eventName))
+        {
+            actionDict.Add(eventName, action);
+        }
+        else {
+            actionDict[eventName] -= action;
+            actionDict[eventName] += action;
+        }
+    }
+    public void UnRegistEvent(string eventName, Action action) {
+        if (actionDict.ContainsKey(eventName)) {
+            actionDict[eventName] -= action;
+            if (actionDict[eventName] == null) {
+                actionDict.Remove(eventName);
+            }
+        }
+    }
+    public void SendEvent(string eventName) {
+        if (actionDict.ContainsKey(eventName)) {
+            actionDict[eventName].Invoke();
+        }
+    }
+}
--- Client/ClawFight/ClawFight/Assets/Scripts/Game/GameManager.cs
+++ Client/ClawFight/ClawFight/Assets/Scripts/Game/GameManager.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+    public MatchProxy matchProxy;
+    public HallPlayer hallPlayer;
     // Start is called before the first frame update
     void Start()
     {
         TcpConnect tcpConnect = new TcpConnect();
         tcpConnect.Start();
+        instance = this;
+
+        matchProxy = new MatchProxy();
+        hallPlayer = new HallPlayer();
     }
 
     // Update is called once per frame
