c834f715afdeaca3c2c5c345536065f9111a9288
XBWXBWXBW <949030585@qq.com>
2020-03-02 10:59:52 +0800

    添加人物移动的代码

--- Client/ClawFight/ClawFight/Assets/Scripts/Player/LocalPlayer.cs
+++ Client/ClawFight/ClawFight/Assets/Scripts/Player/LocalPlayer.cs
@@ -8,23 +8,25 @@ public class LocalPlayer : PlayerMonoBase
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
     private Vector3 moveDirection = Vector3.zero;
+    CharacterController characterController;
+    private const string HORIZONTAL = "Horizontal", VERTICAL = "Vertical", JUMP = "Jump";
 
     void Start()
     {
-
+        characterController = GetComponent<CharacterController>();
     }
     void Update()
     {
-        CharacterController controller = GetComponent<CharacterController>();
-        if (controller.isGrounded)
-        {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (characterController.isGrounded) {
+            moveDirection = new Vector3(Input.GetAxis(HORIZONTAL), 0, Input.GetAxis(VERTICAL));
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= speed;
-            if (Input.GetButton("Jump"))
+
+            if (Input.GetButton(JUMP)) {
                 moveDirection.y = jumpSpeed;
+            }
         }
         moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
+        characterController.Move(moveDirection * Time.deltaTime);
     }
 }
